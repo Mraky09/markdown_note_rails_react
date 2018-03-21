f138e333b334908e0c4773055355dafe1708457c
Mraky09 <anhtnt09@gmail.com>
2018-03-22 01:13:53 +0700

    Install Bootstrap

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,24 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+
+const manifestPlugin = environment.plugins.get('Manifest')
+manifestPlugin.opts.writeToFileEmit = false
+
+environment.plugins.prepend({
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    'window.Tether': 'tether',
+    Popper: ['popper.js', 'default']
+  })
+})
+
+environment.plugins.insert('CommonChunkVendor',
+  new webpack.optimize.CommonsChunkPlugin({
+    name: 'vendor',
+    minChunks: (module) => module.context && module.context.indexOf('node_modules') !== -1
+  })
+, { before: 'manifest' })
 
 module.exports = environment
